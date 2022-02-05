c0705b66f81446b0392372ab39782c4d06a235f5
Usama Shaikh <skusama761@gmail.com>
2022-02-05 16:45:11 +0530

    problem3 added

--- bin/problem3_List.dart
+++ bin/problem3_List.dart
@@ -1,7 +1,12 @@
 void main() {
   var a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
 
-  for (var i = 0; i < a.length; i++) {
-    a[i] < 5 ? print(a[i]) : null;
-  }
+  // for (var i = 0; i < a.length; i++) {
+  //   a[i] < 5 ? print(a[i]) : null;
+  // }
+
+  print([
+    for (var i in a)
+      if (i < 5) i
+  ]);
 }
