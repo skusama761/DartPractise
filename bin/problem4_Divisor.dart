90f5059a13a1fc9b1e126197011888ecc20248fc
Usama Shaikh <skusama761@gmail.com>
2022-02-05 17:26:19 +0530

    problem4 edited

--- bin/problem4_Divisor.dart
+++ bin/problem4_Divisor.dart
@@ -1,11 +1,20 @@
 import 'dart:io';
 
-void main() {
-  var num1 = int.parse(stdin.readLineSync()!);
+import 'dart:math';
 
-  for (var i = 1; i < 100; i++) {
-    if (num1 % i == 0) {
-      print(i);
+void main() {
+  while (true) {
+    try {
+      print('Enter a number');
+      var num1 = int.parse(stdin.readLineSync()!);
+      for (var i = 1; i < 100; i++) {
+        if (num1 % i == 0) {
+          print(i);
+        }
+      }
+      break;
+    } catch (e) {
+      print('Error occured $e');
     }
   }
 }
